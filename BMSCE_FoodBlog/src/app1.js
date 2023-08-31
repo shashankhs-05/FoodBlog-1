b02e33b7cacd50127c34fbf308b9d8dbd60d3d8b
Shravanth28 <shravanth.cs21@bmsce.ac.in>
2023-08-31 10:29:01 +0530

    he

--- BMSCE_FoodBlog/src/app1.js
+++ BMSCE_FoodBlog/src/app1.js
@@ -77,7 +77,7 @@ app.post('/register', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index1.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.get('/bono', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'bono.html'));
